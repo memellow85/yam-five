7f60ea4d7f5ed009aaf8b14e1aa896fb586629e1
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2021-03-24 16:37:50 +0100

    0.0.1 git

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'no-prototype-builtins': 'off',
+    'vue/no-mutating-props': 'off',
+    'array-callback-return': 'off',
+    'new-cap': 'off',
+    'vue/no-v-html': 'off',
+    'no-useless-constructor': 'off',
+    'no-new': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/html-indent': 'off',
+    'nuxt/no-cjs-in-config': 'off',
+  },
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# Yam Five
+
+> Il gioco consiste nel creare combinazioni con i cinque dadi a disposizione per guardagnare il maggior numero di punti!
+
+## Requirements
+
+- Database firebase
+
+## Configuration
+
+## Getting started
+
+## Usage
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+
+# check lint
+$ yarn lint
+
+# install dependencies with clear cache
+$ yarn update_package
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+## Examples
+
+## Release
+
+## License
+
+MIT
