a55a077bd4a9fc50528f0231c4a3b20797d14849
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2021-06-16 13:57:25 +0200

    v1.0.3 Fix problem CORS

--- app/server/server.js
+++ app/server/server.js
@@ -8,6 +8,17 @@ const io = require('socket.io')(server)
 const Rooms = require('../models/Room')()
 const yamfive = require('./api/yamfive')
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
+  next()
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/yam-five', yamfive)
