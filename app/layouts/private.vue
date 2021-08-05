b97c784487d44a0ecd2313aed7129d606c535d8b
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2021-08-05 16:49:00 +0200

    v1.1.1 Integrate g.a. Integrate performace trace

--- app/layouts/private.vue
+++ app/layouts/private.vue
@@ -11,3 +11,18 @@
     <Alert :message="$t('alert.message')"></Alert>
   </Layout>
 </template>
+
+<script>
+import firebase from '~/server/api/firebase'
+
+export default {
+  mounted() {
+    if (
+      process.env.NUXT_ENV_NODE_ENV === 'production' ||
+      process.env.NUXT_ENV_NODE_ENV === 'beta'
+    ) {
+      firebase.analytics()
+    }
+  },
+}
+</script>
