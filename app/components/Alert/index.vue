c29134740f0c2566f9007b7c3981abda9a8c795e
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2022-02-04 18:34:17 +0100

    1.3.2 - fix update

--- app/components/Alert/index.vue
+++ app/components/Alert/index.vue
@@ -64,7 +64,7 @@ export default {
       newVersion: (state) => state.newVersion,
     }),
     multipleButton() {
-      return this.typeModal === 'share' || this.typeModal === 'update'
+      return this.typeModal === 'share' || !this.typeModal
     },
     useVersion() {
       return this.typeModal === 'update'
