89a58f314077ca0b0d67daa8ad6e5820929d954c
Emanuele Barachetti <emanuele.barachetti@serioplast.com>
2022-02-18 07:46:20 +0100

    1.4.0 - Bugfix multiplayer

--- app/server/server.js
+++ app/server/server.js
@@ -14,6 +14,17 @@ const webhook = new IncomingWebhook(WEBHOOK_URL)
 
 const Rooms = require('../models/Room')()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
+  next()
+})
+
 app.use(sslRedirect.default())
 
 const leftRoom = (id, socket) => {
